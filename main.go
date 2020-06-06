f5e56b98754832dc8ed4f2ee5e0e37243404e5ea
Emanuel Estrada Larios <emamex98@gmail.com>
2020-06-05 20:44:57 -0500

    Draw floor plan, walls & exits from matrix.

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/imdraw"
+	"github.com/faiface/pixel/pixelgl"
+	"golang.org/x/image/colornames"
 )
 
 /*
@@ -12,35 +17,103 @@ Matrix building representation:
 	3 represents exit
 */
 var building = [12][12]int{
-			   //0 1 2 3 4 5 6 7 8 9 10 11
-				{1,1,1,1,1,1,1,1,1,1,1,1},//0
-				{1,0,0,0,1,0,0,1,0,0,0,1},//1
-				{1,2,1,1,1,1,0,1,1,1,2,1},//2
-				{1,0,0,0,0,0,0,0,0,0,0,1},//3
-				{1,1,1,1,1,0,1,1,1,1,1,1},//4
-				{1,0,0,0,0,0,0,0,0,0,0,1},//5
-				{1,0,1,1,0,0,0,0,2,1,0,1},//6
-				{1,1,1,1,1,0,0,1,1,1,1,1},//7
-				{1,0,0,0,0,0,0,0,0,0,0,1},//8
-				{1,2,1,1,1,0,0,1,1,1,2,1},//9
-				{1,0,0,0,1,0,0,1,0,0,0,1},//10
-				{1,1,1,1,1,1,1,1,1,1,1,1},//11
-				}
+	//0 1 2 3 4 5 6 7 8 9 10 11
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, //0
+	{1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1}, //1
+	{1, 2, 1, 1, 1, 1, 0, 1, 1, 1, 2, 1}, //2
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, //3
+	{1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1}, //4
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, //5
+	{1, 0, 1, 1, 0, 0, 0, 0, 2, 1, 0, 1}, //6
+	{1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1}, //7
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, //8
+	{1, 2, 1, 1, 1, 0, 0, 1, 1, 1, 2, 1}, //9
+	{1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1}, //10
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, //11
+}
 
-func printBuilding(){
-	for _,row := range building {
-		for _,col := range row{
+func printBuilding() {
+	for _, row := range building {
+		for _, col := range row {
 			fmt.Print(col)
 		}
 		fmt.Println()
 	}
 }
+
 var (
 	numberOfPeople int = 5
 )
 
 type person struct {
 	speed float32
+}
+
+func createWindow() *pixelgl.Window {
+
+	// Specify configuration window
+	cfg := pixelgl.WindowConfig{
+		Title:  "Eathquake Evacuation Simulator",
+		Bounds: pixel.R(0, 0, 840, 840),
+		VSync:  true,
+	}
+
+	// Create a new window
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	win.Clear(colornames.Black)
+	return win
+}
+
+func drawFloor() *imdraw.IMDraw {
+
+	floor := imdraw.New(nil)
+
+	floor.Color = colornames.Lightgray
+	floor.Push(pixel.V(60, 60))
+	floor.Push(pixel.V(780, 780))
+	floor.Rectangle(0)
+
+	var x = 60.0
+	var y = 60.0
+
+	for i := len(building) - 1; i >= 0; i-- {
+		for _, col := range building[i] {
+			if col == 1 {
+				floor.Color = colornames.Gray
+				floor.Push(pixel.V(x, y))
+				floor.Push(pixel.V(x+60.0, y+60.0))
+				floor.Rectangle(0)
+			} else if col == 3 {
+				floor.Color = colornames.Red
+				floor.Push(pixel.V(x, y))
+				floor.Push(pixel.V(x+60.0, y+60.0))
+				floor.Rectangle(0)
+			}
+			x += 60.0
+		}
+		x = 60.0
+		y += 60.0
+	}
+
+	return floor
+}
+
+func run() {
+
+	win := createWindow()
+	floor := drawFloor()
+
+	win.Clear(colornames.Black)
+	floor.Draw(win)
+	win.Update()
+
+	for !win.Closed() {
+
+	}
 }
 
 func main() {
@@ -68,4 +141,6 @@ func main() {
 
 		}
 	}
+
+	pixelgl.Run(run)
 }
