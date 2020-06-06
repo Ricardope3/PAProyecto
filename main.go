f38bf32aa42180838434b73960866ceea694b720
Georgina <ginaglez91@gmail.com>
2020-06-06 17:46:44 -0500

    start of searchPath func

--- main.go
+++ main.go
@@ -2,7 +2,19 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"math"
+	"time"
 )
+
+type person struct {
+	speed float32
+}
+
+type coordinate struct {
+	row int
+	col int
+}
 
 /*
 Matrix building representation:
@@ -12,7 +24,7 @@ Matrix building representation:
 	3 represents exit
 */
 var (
-	building = [12][12]int{
+	building = [][]int{
 			   //0 1 2 3 4 5 6 7 8 9 10 11
 				{1,1,1,1,1,1,1,1,1,1,1,1},//0
 				{1,0,0,0,1,0,0,1,0,0,0,1},//1
@@ -28,11 +40,33 @@ var (
 				{1,1,1,1,1,1,1,1,1,1,1,1},//11
 				}
 	numberOfPeople int
+	numberOfExits int
+	exits [5]coordinate
+	past [12][12][2]bool //0:whether it has been visited 1:whether it formas part of the path
 )
+
+func initializePast(){
+	for i,row := range past{
+		for j,_ := range row{
+			past[i][j][0] = false
+			past[i][j][1] = true
+		}
+	}
+}
+
 func printBuilding(){
 	for _,row := range building {
 		for _,col := range row{
 			fmt.Print(col)
+		}
+		fmt.Println()
+	}
+}
+
+func printPast(){
+	for _,row := range past {
+		for _,col := range row{
+			fmt.Print("[",col[0],",",col[1],"]")
 		}
 		fmt.Println()
 	}
@@ -49,12 +83,131 @@ func getNumOfPeople(){
 	fmt.Println("Number of people: ", numberOfPeople)
 }
 
-type person struct {
-	speed float32
+func insertExit(floor [][]int, sideExits []int, side, indexExit, lenF int) bool {
+	switch side {
+	case 0:
+		if (floor[0][indexExit] == 3 || floor[1][indexExit] == 1){
+			return false
+		}
+		floor[0][indexExit] = 3
+		exits[numberOfExits] = coordinate{row:0 , col: indexExit}
+		numberOfExits++
+	case 1:
+		if (floor[indexExit][lenF-1]==3 || floor[indexExit][lenF-2] == 1){
+			return false
+		}
+		floor[indexExit][lenF-1] = 3
+		exits[numberOfExits] = coordinate{row:indexExit , col: lenF-1}
+		numberOfExits++
+	case 2:
+		if (floor[lenF-1][indexExit]==3 || floor[lenF-2][indexExit] == 1){
+			return false
+		}
+		floor[lenF-1][indexExit] = 3
+		exits[numberOfExits] = coordinate{row: lenF-1, col: indexExit}
+		numberOfExits++
+	case 3:
+		if (floor[indexExit][0]==3 || floor[indexExit][1] == 1){
+			return false
+		}
+		floor[indexExit][0] = 3
+		exits[numberOfExits] = coordinate{row: indexExit, col: 0}
+		numberOfExits++
+	default:
+		sideExits[side]++
+	}
+	return true
+}
+
+func generateExits(floor [][]int){
+	sideExits := make([]int, 4)
+	lenF := len(floor)
+	rand.Seed(time.Now().UnixNano())
+	nExits := rand.Intn(5) + 1
+	for i:=0;i<nExits;{
+		side := rand.Intn(4)
+		indexExit := rand.Intn(lenF)
+		if (sideExits[side]>1){
+			continue
+		}
+		valid := insertExit(floor, sideExits, side, indexExit, lenF)
+		if(valid){
+			i++
+		}
+	}
+	fmt.Println("Number of exits: ", numberOfExits)
+}
+
+func distance(a coordinate, b coordinate) float64{
+	res := math.Pow(float64(a.col) - float64(b.col), 2) + math.Pow(float64(a.row) - float64(b.row), 2)
+	res = math.Sqrt(res)
+	return res
+}
+
+func findClosestExit(position coordinate) int{
+	targetExit := 0
+	dist := distance(position,exits[0]) 
+	for i := 1; i < numberOfExits; i++ {
+		d :=distance(position,exits[i])
+		if d < dist {
+			targetExit = i
+			dist = d 
+		}
+	}
+	return targetExit
+}
+
+func validate(row int, col int) bool{
+	if row < 0 || row >= len(building) {
+		return false;
+	}//not a valid row
+	if col < 0 || col >= len(building[row]) {
+		return false;
+	}//not a valid column
+	if building[row][col] == 1 {
+		return false;
+	}//wall/obstacle
+	if past[row][col][0] == true {
+		return false;
+	}//visited
+	return true;
+}
+
+func searchPath(row int, col int) bool{
+	position := coordinate{row,col}
+	e := findClosestExit(position) //index of target exit
+	initializePast()
+	past[row][col][0] = true;//mark visited
+	if row == exits[e].row && col == exits[e].col { //reached end
+		return true;
+	}else{
+		if validate(row,col+1) {
+			searchPath(row,col+1);
+		}//available path at right
+		if validate(row,col-1) {
+			searchPath(row,col-1);
+		}//available path at left
+		if validate(row+1,col) {
+			searchPath(row+1,col);
+		}//available path down
+		if validate(row-1,col) {
+			searchPath(row-1,col);
+		}//available path up
+		past[row][col][1] = false
+		return false//not the way
+	}
+
 }
 
 func main() {
+	initializePast()
+	printPast()
+	generateExits(building)
+	printBuilding()
 	getNumOfPeople()
+	searchPath(2,1)
+	printPast()
+	/*
 	//Slice containing all the trapped people inside the building
 	//TODO: POPULATE THIS ARRAY
 	trapped := make([]person, numberOfPeople)
@@ -77,5 +230,5 @@ func main() {
 			//the canvas should be redrawn
 
 		}
-	}
+	}*/
 }
